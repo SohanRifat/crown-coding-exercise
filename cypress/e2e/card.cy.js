62f80c34314449afa471d4b662baf3b7b234aedb
SohanRifat <souzrat94@gmail.com>
2023-07-26 07:31:19 -0400

    cards

---
+++ cypress/e2e/card.cy.js
@@ -0,0 +1,51 @@
+describe("Blackjack Game Test", () => {
+  it("should check for blackjack", () => {
+    cy.visit("https://deckofcardsapi.com/");
+
+    // Confirm the site is up
+    cy.contains("Deck of Cards");
+
+    // Get a new deck
+    cy.request("GET", "https://deckofcardsapi.com/api/deck/new/").then((response) => {
+      const deckId = response.body.deck_id;
+
+      // Shuffle the deck
+      cy.request("GET", `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+
+      // Deal three cards to each of two players
+      cy.request("GET", `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=6`).then((response) => {
+        const cards = response.body.cards;
+
+        // Check whether either has blackjack
+        const player1Hand = [cards[0], cards[1], cards[2]];
+        const player2Hand = [cards[3], cards[4], cards[5]];
+
+        const player1Score = calculateHandScore(player1Hand);
+        const player2Score = calculateHandScore(player2Hand);
+
+        // Check for blackjack (score of 21)
+        if (player1Score === 21) {
+          cy.log("Player 1 has blackjack!");
+        }
+        if (player2Score === 21) {
+          cy.log("Player 2 has blackjack!");
+        }
+      });
+    });
+  });
+});
+
+// Helper function to calculate the score of a hand
+function calculateHandScore(hand) {
+  let score = 0;
+  for (const card of hand) {
+    if (["KING", "QUEEN", "JACK"].includes(card.value)) {
+      score += 10;
+    } else if (card.value === "ACE") {
+      score += 11;
+    } else {
+      score += parseInt(card.value);
+    }
+  }
+  return score;
+}
